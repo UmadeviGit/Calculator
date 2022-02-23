86526344a2f54a4d5cc9cd7ba48ea8f2057649f6
UmadeviGit <94173120+UmadeviGit@users.noreply.github.com>
2022-02-23 12:29:53 +0530

    Update Calculator.java
    added new line

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
     public static void main(String args[]) {
        	System.out.println("\n ---> Hello Git 123 ~ ");
         System.out.println("\n ---> Hello Git 456 ~ ");
+  System.out.println("\n ---> Hello Git 890 ~ ");
  
         // new Calculator();
     }
